a0f0e3fef648c76aa3e1c4acff38b38b5378509e
Viktor Miric <viktorm47@gmail.com>
2024-07-13 14:46:18 +0200

    cropping mock image inside a phone template

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import ImageComponent from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { Rnd } from "react-rnd";
 
@@ -12,26 +13,73 @@ const mockImg = {
   src: "/testimonials/4.png",
 };
 
-const aspect = 1080 / 1920;
+const aspect = mockImg.width / mockImg.height;
 
 export default function Page() {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [sectionWidth, setSectionWidth] = useState<number | null>(null);
+  const designContainer = useRef<HTMLDivElement | null>(null);
+  const phoneContainer = useRef<HTMLDivElement | null>(null);
+  const [imageDimensions, setImageDimensions] = useState<{
+    width: number;
+    height: number;
+  }>({
+    width: 230,
+    height: 230 / aspect,
+  });
+  const [imagePosition, setImagePosition] = useState<{
+    x: number;
+    y: number;
+  }>({
+    x: 235,
+    y: (445 - 230 / aspect) / 2,
+  });
 
   useEffect(() => {
-    setSectionWidth(ref.current!.offsetWidth);
-    // console.log(ref.current!.offsetWidth);
+    // console.log(designContainer.current!.getBoundingClientRect().left);
+    // console.log(phoneContainer.current!.getBoundingClientRect().left);
   }, []);
+
+  async function saveConfiguration() {
+    const canvas = document.createElement("canvas");
+    canvas.height = phoneContainer.current!.clientHeight;
+    canvas.width = phoneContainer.current!.clientWidth;
+    const ctx = canvas.getContext("2d");
+    const image = new Image();
+    image.src = mockImg.src;
+
+    const sourceX =
+      imagePosition.x -
+      (phoneContainer.current!.getBoundingClientRect().left -
+        designContainer.current!.getBoundingClientRect().left);
+    const sourceY =
+      imagePosition.y -
+      (phoneContainer.current!.getBoundingClientRect().top -
+        designContainer.current!.getBoundingClientRect().top);
+
+    // console.log(imageDimensions);
+    image.onload = () => {
+      ctx!.drawImage(
+        image,
+        sourceX,
+        sourceY,
+        imageDimensions.width,
+        imageDimensions.height,
+      );
+      console.log(canvas.toDataURL());
+    };
+  }
 
   return (
     <div className="my-6 grid w-full grid-cols-3 gap-2">
       <div
-        ref={ref}
+        ref={designContainer}
         className="relative col-span-2 flex items-center justify-center overflow-hidden rounded-3xl border-2 border-dashed border-zinc-300 bg-background"
       >
-        <div className="pointer-events-none relative w-48 select-none">
+        <div
+          ref={phoneContainer}
+          className="pointer-events-none relative w-48 select-none"
+        >
           <AspectRatio ratio={896 / 1831} className="z-50">
-            <Image
+            <ImageComponent
               src="/phone-template.png"
               sizes="33vw"
               fill
@@ -42,6 +90,23 @@ export default function Page() {
           <div className="absolute inset-[2px] z-10 rounded-[24px] shadow-[0_0_0_9999px_rgba(255,255,255,0.6)]"></div>
         </div>
         <Rnd
+          onResizeStop={(_, __, ref, ___, position) => {
+            setImageDimensions({
+              width: ref.clientWidth,
+              height: ref.clientHeight,
+            });
+            setImagePosition({
+              x: position.x,
+              y: position.y,
+            });
+            // console.log(position);
+          }}
+          onDragStop={(_, data) =>
+            setImagePosition({
+              x: data.x,
+              y: data.y,
+            })
+          }
           lockAspectRatio
           resizeHandleClasses={{
             bottomLeft: resizeHandleStyle,
@@ -57,7 +122,7 @@ export default function Page() {
             height: 230 / aspect,
           }}
         >
-          <Image
+          <ImageComponent
             priority
             alt="img"
             className="pointer-events-none select-none"
@@ -67,7 +132,9 @@ export default function Page() {
           />
         </Rnd>
       </div>
-      <div className="bg-blue-500">options</div>
+      <div className="rounded-xl border bg-background">
+        <Button onClick={() => saveConfiguration()}>Continue</Button>
+      </div>
     </div>
   );
 }
