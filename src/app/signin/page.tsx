31c0c0c09cf0bd7c231099bd442dd0a5c6b1ecd1
Viktor Miric <viktorm47@gmail.com>
2024-07-23 22:59:48 +0200

    sign in styling

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 import { useEffect, useState } from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 export default function Page() {
   const [loading, setLoading] = useState(false);
@@ -15,15 +17,38 @@ export default function Page() {
 
   return (
     <div className="absolute top-0 flex h-screen w-screen items-center justify-center bg-secondary">
-      <div className="rounded-xl bg-background p-8">
-        <button
-          onClick={() => {
-            setLoading(true);
-            signIn("github", { callbackUrl: callbackUrl });
-          }}
-        >
-          Sign In
-        </button>
+      <div className="flex flex-col gap-1 rounded-xl bg-background p-12">
+        <p className="text-2xl font-bold">
+          Case<span className="text-primary">Maker</span>
+        </p>
+        <p className="text-muted-foreground">Please sign in to continue.</p>
+        <div className="mb-4 h-px w-full bg-muted-foreground" />
+        <div className="flex flex-col gap-4">
+          <Button
+            size="lg"
+            className="gap-3 border border-foreground py-6 text-lg"
+            variant="ghost"
+            onClick={() => {
+              setLoading(true);
+              signIn("github", { callbackUrl: callbackUrl });
+            }}
+          >
+            <FaGithub className="size-8" />
+            Sign In with Github
+          </Button>
+          <Button
+            size="lg"
+            className="gap-3 border border-foreground py-6 text-lg"
+            variant="ghost"
+            onClick={() => {
+              setLoading(true);
+              // signIn("github", { callbackUrl: callbackUrl });
+            }}
+          >
+            <FaGoogle className="size-8" />
+            Sign In with Google
+          </Button>
+        </div>
       </div>
     </div>
   );
