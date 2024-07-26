7c3a83e23b06cb12e67098383d58bc02c4ce9155
Viktor Miric <viktorm47@gmail.com>
2024-07-26 12:28:34 +0200

    styled email component

--- src/emails/OrderPaid.tsx
+++ src/emails/OrderPaid.tsx
@@ -1,52 +1,109 @@
 import {
   Body,
   Button,
+  Column,
   Container,
   Head,
   Hr,
   Html,
-  Img,
   Preview,
+  Row,
   Section,
   Text,
 } from "@react-email/components";
 import * as React from "react";
 
-interface KoalaWelcomeEmailProps {
-  userFirstname: string;
+interface OrderPaidEmailProps {
+  OrderNumber: string;
+  OrderDate: string;
+  OrderStatus: string;
+  Name: string;
+  Address: string;
+  City: string;
+  ZipCode: string;
+  PhoneModel: string;
+  CaseType: string;
+  Finish: string;
 }
 
-export default function OrderPaid({ userFirstname }: KoalaWelcomeEmailProps) {
+export default function OrderPaid({
+  OrderNumber,
+  OrderDate,
+  OrderStatus,
+  Name,
+  Address,
+  City,
+  ZipCode,
+  PhoneModel,
+  CaseType,
+  Finish,
+}: OrderPaidEmailProps) {
   return (
     <Html>
       <Head />
       <Preview>
-        The sales intelligence platform that helps you uncover qualified leads.
+        Your custom phone case has been paid and will be shipped shortly.
       </Preview>
       <Body style={main}>
         <Container style={container}>
-          <Text style={paragraph}>
+          <Text style={logo}>
             Case<span style={span}>Maker</span>
           </Text>
-          <Text style={paragraph}>Hi {userFirstname},</Text>
           <Text style={paragraph}>
-            Welcome to Koala, the sales intelligence platform that helps you
-            uncover qualified leads and close deals faster.
-          </Text>
-          <Section style={btnContainer}>
-            <Button style={button} href="https://getkoala.com">
-              Get started
-            </Button>
-          </Section>
-          <Text style={paragraph}>
-            Best,
-            <br />
-            The Koala team
+            Your custom phone case has been paid and will be shipped shortly.
           </Text>
           <Hr style={hr} />
-          <Text style={footer}>
-            470 Noor Ave STE B #1148, South San Francisco, CA 94080
+
+          <Row style={{ display: "inline-flex" }}>
+            <Column style={{ width: "130px" }}>
+              <Text>
+                <strong>Order ID</strong>
+              </Text>
+              <Text>{OrderNumber}</Text>
+            </Column>
+            <Column style={{ width: "130px" }}>
+              <Text>
+                <strong>Order Date</strong>
+              </Text>
+              <Text>{OrderDate}</Text>
+            </Column>
+            <Column>
+              <Text>
+                <strong>Order Status</strong>
+              </Text>
+              <Text>{OrderStatus}</Text>
+            </Column>
+          </Row>
+          <Hr style={hr} />
+          <Section>
+            <Text style={paragraph}>
+              <strong>Shipping to: {Address}</strong>
+            </Text>
+            <Text>
+              {Address}, {City}, {ZipCode}
+            </Text>
+          </Section>
+          <Hr style={hr} />
+          <Text style={paragraphSmall}>
+            <strong>Configuration details</strong>
           </Text>
+          <Text style={paragraphSmall}>Phone model: {PhoneModel}</Text>
+          <Text style={paragraphSmall}>Case type: {CaseType}</Text>
+          <Text style={paragraphSmall}>Finish: {Finish}</Text>
+          <Section style={btnContainer}>
+            <Button
+              style={button}
+              href={`${process.env.NEXT_PUBLIC_SERVER_URL}/my-configurations`}
+            >
+              View your case
+            </Button>
+          </Section>
+          <Hr style={hr} />
+          <Text style={footer}>
+            Please contact us if you have any questions. (If you reply to this
+            email, we won&apos;t be able to see it.){" "}
+          </Text>
+          <Text style={footer}>© 2024 CaseMaker</Text>
         </Container>
       </Body>
     </Html>
@@ -60,7 +117,7 @@ const main = {
 };
 
 const span = {
-  color: "#5F51E8",
+  color: "#e11d48",
 };
 
 const container = {
@@ -69,7 +126,9 @@ const container = {
 };
 
 const logo = {
-  margin: "0 auto",
+  fontSize: "24px",
+  fontWeight: "bold",
+  lineHeight: "26px",
 };
 
 const paragraph = {
@@ -77,12 +136,17 @@ const paragraph = {
   lineHeight: "26px",
 };
 
+const paragraphSmall = {
+  fontSize: "16px",
+  lineHeight: "14px",
+};
+
 const btnContainer = {
   textAlign: "center" as const,
 };
 
 const button = {
-  backgroundColor: "#5F51E8",
+  backgroundColor: "#e11d48",
   borderRadius: "3px",
   color: "#fff",
   fontSize: "16px",
@@ -94,7 +158,7 @@ const button = {
 
 const hr = {
   borderColor: "#cccccc",
-  margin: "20px 0",
+  margin: "10px 0",
 };
 
 const footer = {
